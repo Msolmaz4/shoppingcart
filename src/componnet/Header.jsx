d317da92e053672b9c6ca544af4f07f843535f63
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-04-05 23:59:47 +0200

    nav

--- src/componnet/Header.jsx
+++ src/componnet/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Menu from "./svg/bars-solid.svg";
 import Cart from "./svg/cart-flatbed-solid.svg";
 import Close from "./svg/circle-xmark-solid.svg";
@@ -11,21 +11,32 @@ import './css/header.css'
 
 
 const Header = () => {
+
+
+const [toggle ,setToggle] = useState(false)
+
+const menuToggle =()=>{
+    setToggle({
+        toggle:!toggle
+    })
+}
+
   return (
     <header>
-      <div>
+      <div className="menu" onClick={menuToggle}>
         <img src={Menu} alt="" width="20px" />
       </div>
       <div className="logo">
           <h1><Link to ='./'>Nike</Link></h1>
       </div>
       <nav>
-          <ul>
-              <li><Link to=''>Home</Link></li>
-              <li><Link to=''>Product</Link></li>
-              <li><Link to=''>Contact</Link></li>
-              <li><Link to=''>About</Link></li>
-              <li className="close" >
+          <ul className={toggle ? 'toggle' : ''}>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/product'>Product</Link></li>
+              <li><Link to='/contact'>Contact</Link></li>
+              <li><Link to='/about'>About</Link></li>
+              <li><Link to='/login'>login</Link></li>
+              <li className="close"onClick={menuToggle} >
                   <img src={Close} alt='' width='20px'/>
               </li>
           </ul>
