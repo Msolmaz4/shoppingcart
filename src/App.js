3a14a5b0eafedf84bcc5503ad7c98afac0003911
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-04-06 23:24:13 +0200

    CONtext class weil state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import {BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 import Header from './componnet/Header';
 import Section from './componnet/Section';
-
+import {DataProvider}  from  './componnet/Context'
 function App() {
   return (
-    <Router>
+    <DataProvider>
+<Router>
     <div>
 
 <Header/>
 <Section/>
     </div>
     </Router>
+
+
+    </DataProvider>
+    
   );
 }
 
